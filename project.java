7af995b4c42368546f88f1c6c4dfd4e90fdfcf72
Dream-HUN <48732172+Dream-HUN@users.noreply.github.com>
2019-03-20 19:18:59 +0100

    Add files via upload

---
+++ project.java
@@ -0,0 +1,68 @@
+package cs;
+
+public class project {
+	
+	private class Elem{
+		public int data;
+		public Elem right;
+		public Elem left;
+		public Elem(int a) { data = a; right = null; left = null; }
+	}
+	
+	private class BinaryTree{
+		private Elem e;
+		
+		public void AddData(int x) {
+			if(e != null) {
+				AddData(x, e);
+			} else {
+				e = new Elem(x);
+			}
+		}
+		
+		public void AddData(int x, Elem root) {
+			if(root.data > x) { 
+				if(root.left == null) root.left = new Elem(x); else AddData(x, root.left); 
+				} else { 
+					if(root.right == null) root.right = new Elem(x); else AddData(x, root.right); 
+					}
+		}
+		
+		public void InOrder() {
+			if(e != null) InOrder(e);
+		}
+		
+		public void InOrder(Elem root) {
+			if(root.left != null) InOrder(root.left);
+			System.out.printf("%d ",root.data);
+			if(root.right != null) InOrder(root.right);
+		}
+	}
+	
+	
+	public project() {
+		
+		BinaryTree t = new BinaryTree();
+		t.AddData(5);
+		t.AddData(2);
+		t.AddData(1);
+		t.AddData(4);
+		t.AddData(6);
+		t.AddData(7);
+		t.AddData(8);
+		t.AddData(9);
+		t.AddData(10);
+		t.InOrder();
+		
+		/*Lista l = new Lista();
+		l.liste();
+		System.out.println();
+		l.delv();
+		l.liste();*/
+	}
+	
+	public static void main(String[] t) {
+		project p = new project();
+	}
+}
+
